b02517dee2ca73cdedbd031a870bd13bfe9e537f
Jia Cheng <cif-jiacheng.teo@ninjavan.co>
2021-04-13 22:59:49 +0800

    Update built-in-functions.py

--- functions/built-in-functions.py
+++ functions/built-in-functions.py
@@ -1,13 +1,32 @@
-x = [1, 2, 3, 4, 5]
-print(len(x))
-# >>> 5
+print(0)
+print(1)
+print(2)
+print(3)
+print(4)
+
+for i in range(0, 5):
+    print(i)
+
+'''
+>>> 0
+>>> 1
+>>> 2
+>>> 3
+>>> 4
+>>> 5
+'''
 
 
-x = range(5)
-print(x)
-# >>> range(0, 5)
 
-x = list(range(5))
-print(x)
-# >>> [0, 1, 2, 3, 4]
+# x = [1, 2, 3, 4, 5]
+# print(len(x))
+# # >>> 5
 
+
+# x = range(5)
+# print(x)
+# # >>> range(0, 5)
+
+# x = list(range(5)) 
+# print(x)
+# # >>> [0, 1, 2, 3, 4]
